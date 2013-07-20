74169004a8ff19dcd75edcd03953e01fee8d9760
0723 <aestheticalblur@gmail.com>
2013-07-20 12:53:07 +0900

    added error handling

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -4,14 +4,23 @@
 #include <string>
 #include <set>
 #include <fstream>
+#include "error.h"
 
 class Dictionary {
 	public:
 		Dictionary( const std::string & fname ) {
 			std::ifstream wlist( fname.c_str() );
+			if( ! wlist.is_open() ) {
+				throw ScheckError( "Could not open dictionary file " + fname );	
+			}
+
 			std::string word;
 			while( std::getline( wlist, word ) ) {
 				mWords.insert( word );
+			}
+
+			if( ! wlist.eof() ) {
+				throw ScheckError( "Error reading dictionary file " + fname );	
 			}
 		}	
 		bool Check( const std::string & word ) const {
